5b2068bf4907480de45535b0ae193ea74438295e
thesonproductions <96475289+thesonproductions@users.noreply.github.com>
2022-11-22 22:39:43 +0700

    update

--- web/views/login.php
+++ web/views/login.php
@@ -45,7 +45,7 @@
             <div class="auth-box bg-dark border-top border-secondary">
                 <div id="loginform">
                     <div class="text-center p-t-20 p-b-20">
-                        <span class="db"><img src="public/master/assets/images/logo.png" alt="logo" /></span>
+                        <span class="db"><img src="public/master/assets/images/logo2.png" alt="logo" /></span>
                     </div>
                     <!-- Form -->
                     <form class="form-horizontal m-t-20" id="loginform" action="index.html">
